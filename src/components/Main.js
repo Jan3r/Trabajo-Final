68508d4fc9f22827619b38c00fab8397095f854a
S1M0N-EST <110996217+S1M0N-EST@users.noreply.github.com>
2022-11-16 08:21:26 -0500

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Tort,Comun,Roja,Cig,Lis,Coco} from "./descripcion/descripcion-modulo";
+import { Serpiente,Culebra, Especies, Lagartijo, Lagarto, Tortuga, Caimanes } from "./Especies/especies-modulo";
+import { Lagart, Lisa, Tortu,Tijo,Caiman,Cieg } from "./imagenes/imagenes";
+
+export const M = () => {
+    
+    return(
+        <div>
+        <Especies/>
+
+        <Tortuga/>
+        <Tortu/>
+        <Tort/>
+
+        <Lagarto/>
+        <Lagart/>
+        <Comun/>
+
+        <Lagartijo/>
+        <Tijo/>
+        <Roja/>
+
+        <Culebra/>
+        <Cieg/>
+        <Cig/>
+
+        <Serpiente/>
+        <Lisa/>
+        <Lis/>
+
+        <Caimanes/>
+        <Caiman/>
+        <Coco/>
+
+        </div>
+        
+
+    )
+    
+}
+
+
