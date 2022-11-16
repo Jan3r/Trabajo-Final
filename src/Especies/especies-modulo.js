6bb0fb1310256691890de1ceaddae0fbd8bb2c6f
S1M0N-EST <110996217+S1M0N-EST@users.noreply.github.com>
2022-11-16 08:11:48 -0500

    Add files via upload

---
+++ src/Especies/especies-modulo.js
@@ -0,0 +1,128 @@
+
+import React from 'react';
+import '../imagenes/css/Especies.css'
+export const Especies = () =>{
+
+    return(
+
+        <div class='Border'>
+    <h1> DISTINTAS ESPECIES DE REPTILES </h1>
+
+    <p> En el reino animal exiten distintos tipos de reptiles con caracteristicas ciertamente
+    interesantes, estos pueden clasificarse como lagartos (saurios), cocodrilos,
+    serpientes (ofidios) y tortugas (quelonios). Esta clase de animales son los ancestros tanto 
+    de los mamíferos como de las aves.</p>
+
+    <p> Los reptiles surgieron de alguna clase de anfibios, pero se desconocen los detalles de sus comienzos. 
+    Los restos de los reptiles más primitivos conocidos han sido hallados a comienzos del Carbonífero Superior. 
+    Eran pequeños animales terrestres con un cierto parecido a los lagartos.</p>
+
+    <p> Poseen una piel gruesa protegida por una capa córnea protectora de escamas. 
+    Su reproducción es ovípara (mediante huevos). Su respiración es pulmonar durante toda su vida
+    (incluido cuando son embriones). Esqueleto formado por 4 extremidades (tetrápodos), 
+    constituido por cráneo osificado y columna vertebral divida en 5 regiones 
+    (cervical, torácica, lumbar, sacra y caudal), como los mamíferos.</p>
+
+    <p> Tienen independencia del medio acuático: adaptación total a la vida terrestre. 
+    Animales de sangres frías o poiquilotermos. </p>
+
+    <p> Se conocen aproximadamente unas 8000 especies de reptiles en todo el mundo y 
+    la ciencia que se encarga de estudiar a estos animales (y a los anfibios) es la herpetología. </p>
+
+   </div>
+      
+   )
+}
+
+export const Tortuga = () => {
+
+return(
+    <div>
+        <h2> Tortugas(Quelonios) </h2>
+
+<h3> Tortuga mora (Testudo graeca) </h3>
+
+<h4> Orden: </h4>
+<p> Quelonia </p>
+
+<h4> Familia: </h4>
+<p> Testudinidae </p>
+    </div>
+    
+)}
+
+export const Lagarto = () => {
+    return(
+        <div>
+        <h2> Lagartos (saurios) </h2>
+
+  <h3> Salamanquesa común (Tarentola mauritanica) </h3>
+
+  <h4> Orden: </h4>
+  <p>Squamata</p>
+
+  <h4> Familia: </h4>
+  <p> Gekkonidae </p>
+        </div>
+    )
+}
+
+export const Lagartijo = () => {
+    return(
+        <div>
+        <h3> Lagartija colirroja (Acanthodactylus erythrurus) </h3>
+
+<h4> Orden: </h4>
+<p>Squamata</p>
+
+<h4> Familia: </h4>
+<p> Lacertidae </p>
+        </div>
+    )
+}
+
+export const Culebra = () => {
+    return (
+        <div>
+    <h2> Serpientes/Culebras (Ofidios) </h2>
+    
+    <h3> Culebrilla ciega (Blanus cinereus) </h3>
+
+<h4> Orden: </h4>
+<p>Squamata</p>
+
+<h4> Familia: </h4>
+<p> Amphisbaenidae </p>
+        </div>
+    )
+}
+
+export const Serpiente = () => {
+    return(
+        <div>
+        <h3> Culebra lisa meridional (Coronella girondica) </h3>
+
+    <h4> Orden: </h4>
+     <p>Squamata</p>
+
+<h4> Familia: </h4>
+<p> Colubridae </p>
+        </div>
+    )
+}
+
+export const Caimanes = () => {
+    return(
+        <div>
+            <h2> Caimanes (Caimaninae) </h2>
+            
+            <h3> Caiman Fissipes (Sauropsida) </h3>
+
+            <h4> Orden: </h4>
+            <p>Crocodilia</p>
+
+            <h4> Familia: </h4>
+            <p>Alligatoridae</p>
+        </div>
+    )
+}
