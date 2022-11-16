94308dfad08cc18c1df79ef6672ed2e92d3dcb77
S1M0N-EST <110996217+S1M0N-EST@users.noreply.github.com>
2022-11-16 08:18:59 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import { Especies } from "./components/Especies/especies-modulo";
 import {Imagenes} from "./components/imagenes/imagenes";
+import {Descripcion} from "./components/descripcion/descripcion-modulo"
+import { M } from './components/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-    <Especies />
+    <div style={{backgroundImage: `url("https://media.istockphoto.com/id/1160725910/es/foto/textura-de-fondo-reptiles-de-cuero-verde.jpg?s=612x612&w=0&k=20&c=_eGYH5oKpVOVQBG3oY68rjuExvtXgrWbQBXkCPLwEPs=")`}}>
+     {/* <Especies />
     <Imagenes />
+    <Descripcion />  */}
+    <M />    
+
     </div>
   );
 }
