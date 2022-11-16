5249eecc8640982138ec996b467a94a0ce3ec075
Jan3r <110995928+Jan3r@users.noreply.github.com>
2022-11-16 09:40:32 -0500

    Add files via upload

--- src/components/imagenes/imagenes.js
+++ src/components/imagenes/imagenes.js
@@ -1,8 +1,69 @@
-export const Imagenes =() => {
+import React from 'react'
+import Mora from './img/Mora.jpg'
+import Salamanquesa from './img/Salamanquesa.jpg'
+import Colirroja from './img/Colirroja.jpg'
+import Ciega from './img/Ciega.jpg'
+import Lisaa from './img/Lisa.jpg'
+import Caimanes from './img/Caiman.jpeg'
+import '../css/imagenes.css'
+
+
+export const Tortu =() => {
 return(
 <>
-<div  className ="imagen">
+<div  className ="imagen-tortu">
+    <img src={Mora}/>
 </div>
 </>
 )
 }
+
+export const Lagart =() => {
+    return(
+    <>
+    <div  className ="imagen-Lagart">
+        <img src={Salamanquesa}/>
+    </div>
+    </>
+    )
+    }
+
+    export const Tijo =() => {
+        return(
+        <>
+        <div  className ="imagen-Tijo">
+            <img src={Colirroja}/>
+        </div>
+        </>
+    )
+    }
+
+    export const Cieg =() => {
+        return(
+        <>
+        <div  className ="imagen-Cieg">
+            <img src={Ciega}/>
+        </div>
+        </>
+    )
+    }
+
+    export const Lisa =() => {
+        return(
+        <>
+        <div  className ="imagen-Lisa">
+            <img src={Lisaa}/>
+        </div>
+        </>
+    )
+    }
+
+    export const Caiman =() => {
+        return(
+        <>
+        <div  className ="imagen-Caiman">
+            <img src={Caimanes}/>
+        </div>
+        </>
+    )
+    }
